be83a8198290eae9dc31746993ff11b375d2b9b8
HKnitesh <niteshchaurasiya1910@gmail.com>
2023-06-18 07:44:10 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+const Mountain01 = document.getElementById("Mountain01");
+const Mountain02 = document.getElementById("Mountain02");
+const MountainDetail = document.getElementById("mountainDetail");
+
+function mountain01(){
+    MountainDetail.style.backgroundImage = "url('/mountaintracking/media/CT_SkillTest_v3-11.jpeg')";
+}
+
+function mountain02() {
+    MountainDetail.style.backgroundImage = "url('/mountaintracking/media/CT_SkillTest_v3-7.jpeg')";
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'robo': ['Roboto Condensed', 'sans-serif'],
+      'oswald': ['Oswald', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
+
